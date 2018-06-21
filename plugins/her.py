a50a9f99e4f83d7079dc1ef0eba487633f3634aa
arpadav <arpadav@gmail.com>
2018-06-21 13:56:55 -0400

    added 'her' command

---
+++ plugins/her.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import nacre
+
+class HerSession:
+
+	def __init__(self, pearl, config):
+		self.pearl = pearl
+		self.hangouts = self.pearl.hangouts
+		self.config = config
+		self.buildHandle()
+
+	def build(self):
+		pass
+
+	def buildHandle(self):
+		messageFilter = nacre.handle.newMessageFilter('^{}+her(\s.*)?$'.format(self.pearl.config['format']))
+		async def handle(update):
+			if nacre.handle.isMessageEvent(update):
+				event = update.event_notification.event
+				if messageFilter(event):
+					await self.respond(event)
+		self.pearl.updateEvent.addListener(handle)
+
+	async def respond(self, event):
+		message = """🌕🌕🌕🌕🌕🌕🌕🌕<br>🌕🌕🌕🌕🌕🌕🌕🌕<br>🌕🌕🌕🌕🌘🌑🌒🌕<br>🌕🌕🌕🌘🌑🌑🌑🌓<br>🌕🌕🌖🌑👁🌑👁🌓<br>🌕🌕🌗🌑🌑👄🌑🌔<br>🌕🌕🌘🌑🌑🌑🌒🌕<br>🌕🌕🌘🌑🌑🌑🌓🌕<br>🌕🌕🌘🌑🌑🌑🌔🌕<br>🌕🌕🌘🌔🌘🌑🌕🌕<br>🌕🌖🌒🌕🌗🌒🌕🌕<br>🌕🌗🌓🌕🌗🌓🌕🌕"""
+		conversation = self.hangouts.getConversation(event=event)
+		await self.hangouts.send(message, conversation)
+
+def load(pearl, config):
+	return HerSession(pearl, config)
