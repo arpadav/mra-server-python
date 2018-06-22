6670571d9aaaf12acbad1f6dc0be31849a1a382c
arpadav <arpadav@gmail.com>
2018-06-22 12:57:45 -0400

    some 8ball response changes

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -5,26 +5,31 @@ import nacre
 class EightBallSession:
 
 	answers = [
-		"It is certain",
-		"It is decidedly so",
-		"Without a doubt",
-		"Yes definitely",
-		"You may rely on it",
-		"As I see it, yes",
-		"Most likely",
-		"Outlook good",
-		"Yes",
-		"Signs point to yes",
-		"Reply hazy try again",
-		"Ask again later",
-		"Better not tell you now",
-		"Cannot predict now",
-		"Concentrate and ask again",
-		"Don't count on it",
-		"My reply is no",
-		"My sources say no",
-		"Outlook not so good",
-		"Very doubtful"
+		"It is certain",				#yes1
+		"It is decidedly so",			#yes2
+		"Without a doubt",				#yes3
+		"Yes definitely",				#yes4
+		"You may rely on it",			#yes5
+		"As I see it, yes",				#yes6
+		"Most likely",					#yes7
+		"Outlook good",					#yes8
+		"Yes",							#yes9
+		"All signs point to yes",		#yes10
+		"Reply hazy try again",			#uncertain1
+		"Better not tell you now",		#uncertain2
+		"Maybe",						#uncertain3
+		"Not sure"						#uncertain4
+		"Why would you even ask that?",	#uncertain5
+		"Don't count on it",			#no1
+		"My reply is no",				#no2
+		"My sources say no",			#no3
+		"Outlook not so good",			#no4
+		"Very doubtful", 				#no5
+		"No",							#no6
+		"Absolutely not",				#no7
+		"Nope",							#no8
+		"All signs point to no", 		#no9
+		"Unlikely"						#no10
 	]
 
 	def __init__(self, pearl, config):
