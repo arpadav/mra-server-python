b36003a21231922192f380019d33a89b83024309
arpadav <arpadav@gmail.com>
2018-06-28 15:31:50 -0400

    oxford dictionary define command

--- plugins/define.py
+++ plugins/define.py
@@ -1,6 +1,6 @@
 import asyncio
-
 import nacre
+import requests, json
 
 class DefineSession:
 
@@ -10,6 +10,7 @@ class DefineSession:
 		self.hangouts = self.pearl.hangouts
 		self.config = config
 		self.buildHandle()
+		self.api = json.loads(open('plugins/data/define.json').read())
 
 	def build(self):
 		pass
@@ -25,7 +26,25 @@ class DefineSession:
 
 	async def respond(self, event):
 		input = event.chat_message.message_content.segment[0].text.split(" ")[1]
-		message = "definition goes here"
+		url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + self.api['lang'] + '/' + input.lower()
+		r = requests.get(url, headers = {'app_id': self.api["app_id"], 'app_key': self.api["app_key"]})
+
+		message = ""
+		ii = 0
+		jj = 0
+		try:
+			for i in r.json()['results'][0]['lexicalEntries']:
+				for j in r.json()['results'][0]['lexicalEntries'][ii]['entries']:
+					if ii == 0 and jj == 0:
+						message = message + str(ii + 1) + "." + str(jj) + ": " + j['senses'][0]['short_definitions'][0]
+					else:
+						message = message + "<br>" + str(ii + 1) + "." + str(jj) + ": " + j['senses'][0]['short_definitions'][0]
+					jj += 1
+				jj = 0
+				ii += 1
+		except:
+			message = "No definitions found."
+
 		conversation = self.hangouts.getConversation(event=event)
 		await self.hangouts.send(message, conversation)
 
